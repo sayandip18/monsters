d581fb62e171167645509debf0d98ef88becaf1d
Sayandip Halder <sayandiph4@gmail.com>
2021-10-28 21:32:59 +0530

    render monsters

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
 
 class App extends Component {
 
@@ -8,28 +8,18 @@ class App extends Component {
     super();
 
     this.state = {
-      string: 'H e l l o'
+      monsters: []
     };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users').then((response) => response.json()).then((users) => this.setState({monsters: users}));
   }
 
   render(){
     return(
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            <p>{this.state.string}</p>
-            <button onClick={() => this.setState({string: 'Hello'})}>change text</button>
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <CardList monsters={this.state.monsters}/>
       </div>
     );
   }

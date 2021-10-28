aa9734310fc4ddc3dc3a09af91d8855031e40bfe
Sayandip Halder <sayandiph4@gmail.com>
2021-10-29 03:12:32 +0530

    search functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component'
 
 class App extends Component {
 
@@ -8,7 +9,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -17,9 +19,17 @@ class App extends Component {
   }
 
   render(){
+    const { monsters, searchField } = this.state;
+    const filteredMonster = monsters.filter(
+      monster => monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return(
       <div className="App">
-        <CardList monsters={this.state.monsters}/>
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={e=>this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonster}/>
       </div>
     );
   }
